47d8a799a9f058fdb561f565fde9fb6267c48109
unkn3wn <reyesfernando151@gmail.com>
2023-01-28 15:06:18 -0800

    third commit

---
+++ function/varibales/calculator.py
@@ -0,0 +1,18 @@
+# x = input("first number? ")
+# y = input("Second Number? ")
+
+# z = int(x) + int(y)
+
+# print(z)
+
+# different way of writing it i am nesting here
+x = int(input("first number? "))
+y = int(input("second Number? "))
+
+print(x+y)
+
+# this is another way of writing it out 
+# x = int(input("first number? "))
+# y = int(input("second Number? "))
+# z = x+y
+# print(z)
--- function/varibales/split.py
+++ function/varibales/split.py
@@ -2,5 +2,4 @@ name = input("whats your name?").strip().title()
 # splitting users name into first name and last name 
 first, last = name.split(" ")
 
-
 print(f"hello {last}")
