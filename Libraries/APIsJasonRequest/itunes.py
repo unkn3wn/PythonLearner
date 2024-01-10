d889b4dac5ccf151c99a9ac6f124235317d68195
unkn3wn <reyesfernando151@gmail.com>
2024-01-09 21:19:12 -0800

    this is how to fetch from a response json

--- Libraries/APIsJasonRequest/itunes.py
+++ Libraries/APIsJasonRequest/itunes.py
@@ -1,10 +1,15 @@
+import json
 import requests
+
 import sys
 
 if len(sys.argv) != 2 :
     sys.exit()
     
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=100&term=" + sys.argv[1])
 
-print(response.json())
+o = response.json()
+
+for result in o["results"]:
+    print(result["trackName"])
